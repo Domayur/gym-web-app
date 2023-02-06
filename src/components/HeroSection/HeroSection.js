f3eb6ee5c9af3174db2f3adf5b2d0dc2ea3dba91
mayur <mayurnandwalkar0@gmail.com>
2023-02-07 00:39:37 +0530

    hero section

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -8,25 +8,30 @@ import { BsFillGeoAltFill, BsFillOctagonFill, BsFillPlayFill, BsGeo, BsGeoFill }
 const HeroSection = () => {
     return (
         <>
-            <section className='hero-section'>
+            <section class='hero-section'>
                 <div class="container">
                     <div class="row gy-4 d-flex justify-content-between">
-                        <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
-                            <h2 class="heading-hero-line" data-aos="fade-up">Find Your Next Workout Destination</h2>
+                        <div class="col-lg-8 order-2 order-lg-1 d-flex flex-column justify-content-center">
+                            <h2 class="heading-hero-line" data-aos="fade-up">Find Your Next <br/>Workout Destination</h2>
                             <p class="paragaraph-hero-line" data-aos="fade-up" data-aos-delay="100">Find the Right Gym for Your Fitness Goals and Preferences</p>
                             <form action="#" class="form-search d-flex align-items-stretch mb-3" data-aos="fade-up" data-aos-delay="200">
-                                <div className='mayur'>
-                                    <img src={LocationImg} alt="location" />
-                                    <input type="text" class="form-control" placeholder={"ZIP code or CitY"} />
+                                <div class='row mayur justify-content-between"'>
+                                    <div class="col-sm-1">
+                                        <img src={LocationImg} alt="location" />
+                                    </div>
+                                    <div class="col-sm-5">
+                                        <input type="text" class="form-control" placeholder="Destination, city, address" />                                        
+                                    </div>
+                                    <div class="col-sm-2 ">
+                                        <button class="btn cst-btn-nav float-right" type="button">Signup</button>
+                                    </div>
                                 </div>
-
-                                <button class="btn cst-btn-nav" type="button">Signup</button>
                             </form>
                             <div>
-                                <button class="btn how-it-works" type="button">How it Works? <span><BsFillPlayFill /></span></button>
+                                <button class="btn how-it-works" type="button">How it Works? <span><BsFillPlayFill className='play-icons' /></span></button>
                             </div>
                         </div>
-                        <div class="col-lg-5 order-1 order-lg-2 hero-img">
+                        <div class="col-lg-4 order-1 order-lg-2 hero-img">
                             <img src={HeroImg} class="img-fluid mb-3 mb-lg-0" alt="" />
                         </div>
                     </div>
