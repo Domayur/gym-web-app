cd4c59aaa7fb64cea27cf816eb2fc1ad4affc791
mayur <mayurnandwalkar0@gmail.com>
2023-02-09 23:46:52 +0530

    changes from dev

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -13,22 +13,27 @@ const HeroSection = () => {
                     <div class="col-lg-12 order-2 order-lg-1 col-sm-8 order-sm-1 d-flex flex-column justify-content-center">
                         {/* <div class="col-lg-8 order-2 order-lg-1 d-flex flex-column justify-content-center"> */}
                             <div class="container mt-5" >
+                            <div class="row" >
+                            <div class="col-lg-6" >
                                 <h2 class="heading-hero-line" data-aos="fade-up">Find Your Next <br />Workout Destination</h2>
                                 <p class="paragaraph-hero-line" data-aos="fade-up" data-aos-delay="100">Find the Right Gym for Your Fitness Goals and Preferences</p>
                                 <div className='rao-new-set justify-content-between'>
                                     <form action="#" class="form-search d-flex align-items-stretch mb-3" data-aos="fade-up" data-aos-delay="200">
-                                        <div class='row marata "'>
-                                            <div class="col-sm-1">
+                                        <div class='marata'>
+                                            <div class="LocationImg">
                                                 <img src={LocationImg} alt="location" />
                                             </div>
-                                            <div class="col-sm-5">
+                                            <div class="desti_input">
+                                                <label for="">where</label>
                                                 <input type="text" class="form-control" placeholder="Destination, city, address" />
                                             </div>
-                                            <div class="col-sm-2 ">
+                                            <div class="search_btn">
                                                 <button class="btn cst-btn-nav float-right" type="button">Signup</button>
                                             </div>
                                         </div>
                                     </form>
+                                </div>
+                                </div>
                                 </div>
                                 <div>
                                     <button class="btn how-it-works" type="button">How it Works? <span><BsFillPlayFill className='play-icons' /></span></button>
