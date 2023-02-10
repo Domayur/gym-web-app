d20e03bf271d79ac0ddbcf6c3badb6839665cd95
mayur <mayurnandwalkar0@gmail.com>
2023-02-10 21:23:50 +0530

    Hero section from mayur dev

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
 import Header from './components/Navbar/Header';
-import HeroSection from './components/HeroSection/HeroSection';
-import PopularDestination from './components/PopularDestination/PopularDestination';
+ import PopularDestination from './components/PopularDestination/PopularDestination';
 import ExploreGymSection from './components/ExploreGym/ExploreGymSection';
 import PoplarGym from './components/PoplarGym/PoplarGym';
 import Footer from './components/Footer/Footer';
 import WhyChooseGym from './components/WhyChooseGym/WhyChooseGym';
+import HeroSection from './components/HeroSection/HeroSection';
 
 function App() {
   return (
--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import HeroImg from '../../assets/images/hero.png'
 import LocationImg from '../../assets/images/location.png'
-import { BsFillPlayFill } from "react-icons/bs";
-import './HeroSection/HeroSection.css'
+import { BsFillPlayFill, BsSearch } from "react-icons/bs";
+import '../HeroSection/HeroSection.css'
 
 const HeroSection = () => {
     return (
@@ -10,30 +10,32 @@ const HeroSection = () => {
             <section class='hero-section'>
                 <div class="container mt-4">
                     <div class="row gy-4 d-flex justify-content-between ">
-                    <div class="col-lg-12 order-2 order-lg-1 col-sm-8 order-sm-1 d-flex flex-column justify-content-center">
-                        {/* <div class="col-lg-8 order-2 order-lg-1 d-flex flex-column justify-content-center"> */}
+                        <div class="col-lg-12 order-2 order-lg-1 col-sm-8 order-sm-1 d-flex flex-column justify-content-center">
+                            {/* <div class="col-lg-8 order-2 order-lg-1 d-flex flex-column justify-content-center"> */}
                             <div class="container mt-5" >
-                            <div class="row" >
-                            <div class="col-lg-6" >
-                                <h2 class="heading-hero-line" data-aos="fade-up">Find Your Next <br />Workout Destination</h2>
-                                <p class="paragaraph-hero-line" data-aos="fade-up" data-aos-delay="100">Find the Right Gym for Your Fitness Goals and Preferences</p>
-                                <div className='rao-new-set justify-content-between'>
-                                    <form action="#" class="form-search d-flex align-items-stretch mb-3" data-aos="fade-up" data-aos-delay="200">
-                                        <div class='marata'>
-                                            <div class="LocationImg">
-                                                <img src={LocationImg} alt="location" />
-                                            </div>
-                                            <div class="desti_input">
-                                                <label for="">where</label>
-                                                <input type="text" class="form-control" placeholder="Destination, city, address" />
-                                            </div>
-                                            <div class="search_btn">
-                                                <button class="btn cst-btn-nav float-right" type="button">Signup</button>
-                                            </div>
+                                <div class="row" >
+                                    <div class="col-lg-6" >
+                                        <h2 class="heading-hero-line" data-aos="fade-up">Find Your Next <br />Workout Destination</h2>
+                                        <p class="paragaraph-hero-line" data-aos="fade-up" data-aos-delay="100">Find the Right Gym for Your Fitness Goals and Preferences</p>
+                                        <div className='rao-new-set justify-content-between'>
+                                            <form action="#" class="form-search d-flex align-items-stretch mb-3" data-aos="fade-up" data-aos-delay="200">
+                                                <div class='marata'>
+                                                    <div class="LocationImg">
+                                                        <img src={LocationImg} alt="location" className="m-1" />
+                                                    </div>
+                                                    <div class="desti_input">
+                                                        <label className='where-class-lable'>where</label><br />
+                                                        <input type="text" class="extra-class-remove-css" placeholder="Destination, city, address" value={"Destination, city, address"} />
+                                                    </div>
+                                                    <div class="row main-div justify-content-between">
+                                                         <div class="col-sm-1">
+                                                            <button class="btn new-class-for-right d-inline" type="button"><BsSearch className='search-chnage-icon-color'/> Search</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </form>
                                         </div>
-                                    </form>
-                                </div>
-                                </div>
+                                    </div>
                                 </div>
                                 <div>
                                     <button class="btn how-it-works" type="button">How it Works? <span><BsFillPlayFill className='play-icons' /></span></button>
